b5a9ae1fa7f0d2579fcee3b37d1f0e7c9d85a5d2
Linkl <68951228+Okairy@users.noreply.github.com>
2021-08-10 10:32:38 +0800

    Update myScript.js

--- BackStage/assets/js/myScript.js
+++ BackStage/assets/js/myScript.js
@@ -1,3 +1,27 @@
 ﻿var website = "https://localhost:44375/";
 ﻿var backStage = "https://localhost:44385/";
 var UI = "https://localhost:44393/";
+
+//MiniDialog自定义弹窗内容级样式（适用于API请求后对返回的结果进行处理时）
+        function MiniDialogBoxInit(msgState, msgResult, msgContent, dialogHeight, fontColor) {
+            if (msgContent == null) {
+                Dialog.msgState(msgResult);
+                $(".mini-dialog-main").css("min-height", "0px");
+                $(".mini-dialog-wrapper-show").css("height", "128px");
+            }
+            if (msgContent != null && msgContent.length < 18) {
+                Dialog.msgState(msgResult, msgContent);
+                $(".mini-dialog-wrapper-show").css("height", "150px");
+            }
+            if (msgContent != null && msgContent.length > 18) {
+                Dialog.msgState(msgResult, msgContent);
+                $(".mini-dialog-wrapper-show").css("height", "180px");
+            }
+
+            if (dialogHeight != null) {
+                $(".mini-dialog-wrapper-show").css("height", dialogHeight);
+            }
+            if (fontColor != null) {
+                $(".mini-dialog-ok>span").css("color", fontColor);
+            }
+        }
